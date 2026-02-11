765a35f31468855f0892c66f6d92df000a3510fa
Vlad <vladislavofficial.812@gmail.com>
2026-02-11 16:09:47 +0300

    refactoring: fix n+1 problem in hotel repository

--- src/main/java/by/vladislav/hotelreservation/repository/HotelRepository.java
+++ src/main/java/by/vladislav/hotelreservation/repository/HotelRepository.java
@@ -1,7 +1,9 @@
 package by.vladislav.hotelreservation.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,8 @@ import by.vladislav.hotelreservation.entity.Hotel;
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
   Optional<Hotel> findByName(String name);
+
+  @EntityGraph(attributePaths = { "rooms" })
+  List<Hotel> findAll();
 
 }
